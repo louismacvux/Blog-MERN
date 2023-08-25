727d2d7d029c6973268a00fdf86192dd209967ff
louismacvux <57550224+louismacvux@users.noreply.github.com>
2023-08-25 14:48:36 -0500

    Delete Modal for FE

---
+++ client/src/components/deletepost.js
@@ -0,0 +1,32 @@
+import React, { useRef, useEffect, useCallback } from "react";
+import { Modal, Button, ModalFooter, ModalHeader, ModalBody } from "reactstrap";
+import axios from "axios";
+
+export default function Delete(props) {
+
+    async function deletePost(id) {
+        try {
+            await axios.delete(`http://localhost:8000/feed/${id}`);
+        } catch (err) {
+            window.alert(`An error has occured: ${err.statusText}\n id:${id}`);
+        }
+    }
+
+    const afterDelete =() => {
+        deletePost(props.id).then(() => window.location.reload(true))
+        props.toggle();
+    }
+
+    return (
+        <Modal isOpen={props.show} toggle={props.toggle}>
+            <ModalHeader>
+            Delete
+            </ModalHeader>
+            <ModalBody>Do you want to delete?</ModalBody>
+            <ModalFooter>
+                <Button color="primary" onClick={props.toggle}>Cancel</Button>
+                <Button color="danger" onClick={() => {afterDelete()}}>Delete</Button>
+            </ModalFooter>
+        </Modal>
+    );
+}
--- client/src/components/feed.js
+++ client/src/components/feed.js
@@ -2,20 +2,35 @@ import React, {useState, useEffect} from 'react'
 //import { useNavigate } from 'react-router-dom'
 import axios from 'axios';
 import '../styling/feed.css';
-import {Container, Row, Col} from 'reactstrap';
+import {Modal,Container, Row, Col, Button} from 'reactstrap';
+import Delete from './deletepost.js';
 
 function Post(props){
+    const [showModal, setShowModal] = useState(false);
+
+    const openModal = ()=>{
+        setShowModal(true);
+    }
+
+    const toggleModal = ()=>{
+        setShowModal(!showModal)
+    }
     return(
-    <Container>
-        <Row className="justify-content-center">
-                <Col className = "col-6 post"> <h4>Title: {props.post.title}</h4></Col>
+        <Container>
+            <Row className="justify-content-center">
+                <Col className="col-6 post"> <h4>Title: {props.post.title}</h4></Col>
                 <Col className="col-6 post align-content-flex-end"><p>Time: {props.post.time}</p></Col>
-                <Col className = "col post"><h5>{props.post.content}</h5></Col>
-        </Row>
-    </Container>
+                <Col className="col post"><h5>{props.post.content}</h5></Col>
+            </Row>
+            <Row>
+                <Col className="col-4"><Button onClick={openModal}>Delete</Button></Col>
+            </Row>
+            <Delete show={showModal} toggle = {toggleModal} id={props.post._id} />
+        </Container>
     )
 
 }
+
 
 export default function Feed(){
      const [feed, setFeed] = useState([]);
@@ -40,7 +55,7 @@ export default function Feed(){
         return feed.map((post) => {
             return (
                 <div className="post">
-                    <Post post = {post} key = {post._id}/>
+                    <Post post = {post}/>
                 </div>
             )
         });
