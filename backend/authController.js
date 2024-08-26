b4e7fbfdb217796af78f62e0125220d85d456c05
louismacvux <57550224+louismacvux@users.noreply.github.com>
2024-08-26 15:41:13 -0500

    Update authController.js

--- backend/authController.js
+++ backend/authController.js
@@ -9,6 +9,7 @@ dotenv.config();
 /* GET Google Authentication API. */
 const googleAuth = async (req, res, next) => {
   console.log("googleAuth")
+  const userRes = {};
   try {
     const code = req.query.code;
     console.log("USER CREDENTIAL -> ", code);
@@ -17,29 +18,31 @@ const googleAuth = async (req, res, next) => {
     
     oauth2Client.setCredentials(googleRes.tokens);
 
-    const userRes = await axios.get(
+    userRes = await axios.get(
       `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${googleRes.tokens.access_token}`
     );
 
-    let user = await User.findOne({ email: userRes.data.email });
-
-    if (!user) {
-      console.log("New User found");
-      user = await User.create({
-        name: userRes.data.name,
-        email: userRes.data.email,
-        image: userRes.data.picture,
-      });
-    }
     
-    req.session.user = user;
-
-    res.send(req.session.user);
-    console.log(`Session set: ${req.sessionID}`);
-    console.log(req.session);
   }catch(err){
     next(err)
   }
+  let user = await User.findOne({ email: userRes.data.email });
+  console.log(`userRes: ${userRes}`)
+  
+  if (!user) {
+    console.log("New User found");
+    user = await User.create({
+      name: userRes.data.name,
+      email: userRes.data.email,
+      image: userRes.data.picture,
+    });
+  }
+
+  req.session.user = user;
+
+  res.send(req.session.user);
+  console.log(`Session set: ${req.sessionID}`);
+  console.log(req.session);
 }; 
 
 export default googleAuth
