51359d03f2039f8aabee1000f8efc3efe2d310bb
louismacvux <57550224+louismacvux@users.noreply.github.com>
2024-09-06 01:11:54 -0500

    edit release note

--- client/src/utils/releasenote.js
+++ client/src/utils/releasenote.js
@@ -1,3 +1,7 @@
-const releaseNote = ```hello```
+const releaseNote = `Some new changes including:
+ - Fix Sidebar issue with option sub-menu and refine scrollbar behavior.
+ - Add more editing tools, including code format and horizontal divider.
+ - Add Shortcut to save note (Ctrl - S).
+ - Add this very place for Release Note.`;
 
 export default releaseNote;
